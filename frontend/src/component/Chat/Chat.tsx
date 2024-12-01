b2ce14d5ea81dae3058c7959010bff6df194b71e
louislombaert <l.lombaert@students.ephec.be>
2024-12-01 16:57:45 +0100

    Creating chat component
    Creating Chat with hardcoded data

---
+++ frontend/src/component/Chat/Chat.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react'
+import {useState, useEffect, ChangeEvent, FormEvent} from 'react';
+import Stack from '@mui/material/Stack';
+import Item from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { FormControl } from '@mui/material';
+
+function Chat() {
+    const [pending, setPending] = useState<boolean>(false);
+    const [messages, setMessages] = useState<Array<string>>([]);
+    const [newMessage, setNewMessage] = useState<string>('');
+    const getMessages = () => setMessages(['message1', 'message2']);
+    // tomodify ?
+    const style = {
+        display: 'flex',
+        justifyContent: 'center',
+        my: 10,
+        mx: 20,
+    }
+    const itemStyle = {
+        border: 1,
+        borderColor: '#030303', // to change
+        padding: 2,
+        borderRadius: 3
+    }
+    const formStyle = {
+        position: 'absolute',
+        bottom: '5%',
+        right: '10%',
+        display: 'flex',
+        flexDirection: 'row'
+    }
+
+    const sendMessage = (event: FormEvent): void => {
+        event.preventDefault();
+        console.log(newMessage);
+    }
+    useEffect(() => {
+        getMessages();
+    }, []);
+    
+    return (
+        <div>
+            <Stack spacing={2} sx={style}>
+                {messages.map((message) => (
+                    <Item sx={itemStyle}>{message}</Item>
+                ))}
+            </Stack>
+            <FormControl onSubmit={sendMessage} sx={formStyle}>
+                <TextField value={newMessage} onChange={(event: ChangeEvent<HTMLInputElement>): void => {
+                    setNewMessage(event.target.value)}}
+                    label="Nouveau message" variant="outlined" />
+                <Button type="submit" variant='contained'>Envoyer</Button>
+            </FormControl>
+        </div>
+    )
+}
+export default Chat
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import Register from './component/Register/Register';
 import reportWebVitals from './reportWebVitals';
+import Chat from './component/Chat/Chat';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,6 +13,7 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     <Register />
+    <Chat />
   </React.StrictMode>
 );
 
